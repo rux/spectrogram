50b6590c16f2ef557d05ed001cb903a85c21d681
Russ Anderson <russ.anderson@yellglobal.com>
2013-12-10 16:54:17 +0000

    blocks now drawn for lower res spectrums rather than 1px lines

--- scripts/spectroscope.js
+++ scripts/spectroscope.js
@@ -24,18 +24,22 @@ require([
 		canvas.height = 0; // this is to stop an old canvas size from interfering if we have a window resize.
 		scopeHeight = document.height; // to fill the screen from top to bottom
 		scopeWidth = document.width;
-		sampleWidth = 2; // larger means faster movement, sadly not higher sampling rate :-( )
+		sampleWidth = 4; // larger means faster movement, sadly not higher sampling rate :-( )
 
         canvas.width=scopeWidth;
         canvas.height=scopeHeight;
 
-		analyser = audio.RealtimeAnalyzer.forPlayer(models.player) // , audio.BAND31);  // default bands is 512.  audio.BAND31 is a suggested
+		analyser = audio.RealtimeAnalyzer.forPlayer(models.player)// , audio.BAND31);  // default bands is 512.  audio.BAND31 is a suggested
 		analyser.addEventListener("audio", doDraw);
 
-		//gradient = chroma.scale(["navy", "purple", "red", "orange", "lightyellow", "white"]).domain([-94,12]);
-		//gradient = chroma.scale(["black", "red", "orange", "yellow", "white", "cyan"]).domain([-94,12]);
-		gradient = chroma.scale(["black", "white"]).domain([-94,12], "log");
 
+		// chroma.js makes colours to a scale.  The actual lowest value for a frequency is -94, but setting it to -90
+		// instead seems to make for ever so slightly cleaner plots, where we discard the very quietest signals (which
+		// were probably noise anyway)
+		// Spotify blog says +0 is the loudest, but the API docs say +12.  API seems to be more right.
+
+		gradient = chroma.scale(["navy", "purple", "red", "orange", "lightyellow", "white"]).domain([-90,12]);
+		//gradient = chroma.scale(["black", "white"]).domain([-90,12], "log");
 
 		window.onresize = function() { init() };  // TODO keep old scope data maybe, rather than complete reset?
 	};
@@ -46,12 +50,13 @@ require([
 
 	var doDraw = function(data) {
 
+		var spectrum = data.audio.spectrum;
+
 		ctx = document.getElementById("canvas").getContext("2d");
 
 		var channelHeight = scopeHeight/2;
-
-		var spectrum = data.audio.spectrum;
 		var spectrumLength = spectrum.left.length;
+		var blockHeight = channelHeight/spectrumLength;
 
         // iterate over the elements from the array
         for (var i = 0; i < spectrumLength; (i++)) {
@@ -61,14 +66,14 @@ require([
 
             // draw the line at the right side of the canvas
             ctx.fillStyle = gradient(valueLeft).hex();
-            ctx.fillRect(scopeWidth - sampleWidth, channelHeight -(spectrumLength/channelHeight) - (channelHeight *i/spectrumLength), sampleWidth, spectrumLength/channelHeight);
+            ctx.fillRect(scopeWidth - (sampleWidth * blockHeight), channelHeight-blockHeight - (blockHeight *i), sampleWidth*blockHeight, blockHeight);
 
             ctx.fillStyle = gradient(valueRight).hex();
-            ctx.fillRect(scopeWidth - sampleWidth, channelHeight + (channelHeight *i/spectrumLength), sampleWidth, spectrumLength/channelHeight);
+            ctx.fillRect(scopeWidth - (sampleWidth * blockHeight), channelHeight             + (blockHeight *i), sampleWidth*blockHeight, blockHeight);
         }
 
         ctx.save();
-        ctx.translate(-sampleWidth,0);
+        ctx.translate(Math.round(-sampleWidth*blockHeight),0);
         ctx.drawImage(canvas, 0, 0);
         ctx.restore();
 	};
